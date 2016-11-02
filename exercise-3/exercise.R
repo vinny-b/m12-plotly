f29ba99fcd61e37c638e940c3beafb17309790e0
Vinny <vinayakbansal@hotmail.com>
2016-11-01 18:21:53 -0700

    Done

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,48 +1,56 @@
 # Install Plotly 
-
+library(plotly)
 
 # Use the plot_ly function to test it out and see that it is working
 # create a variable and pass in the plot_ly function that takes in 
 # the data midwest, sets the x to ~percollege, the color to ~state 
 # and the type to "box" 
-
+x <- plot_ly(midwest, x = ~percollege, color = ~state, type = "box")
 
 # Make two vectors of numbers between 1 and 10, with each vector having 10 numbers in it
-
-
+a <- c(1:10)
+b <- c(1:10)
 # create a data frame of the two vectors 
-
+ab <- data.frame(a,b)
 
 # now create a basic scatterplot using the data frame you just created, passing it into 
 # a variable
-
+scatter <- plot_ly(data = ab, x = a, y = b)
 
 # Call your variable and enjoy your awesome plot! 
-
+scatter
 
 # Now create a new variable styled.scatterplot. Using the same data and scatterplot
 # as from earlier, but you are going to change the markers.  Make it so the 
 # markers are size 10, of a color of your choice.  Finally change the boarder,
 # of the markers to a differnet color with a widthe of 2
-
+style.scatterplot <- plot_ly(data = ab, x = a, y = b, 
+                     marker = list(size = 10, 
+                                   color = 'green', 
+                                   line = list(color = 'blue',
+                                               width = 2))) %>%
 
 # Call the style.scatterplot variable and see what has changed. Finally add
 # add a pipe operator and use the layout function to add a title and set the
 # x and y axis to have zeroline = FALSE 
-
+layout(title = 'StylePlot',
+       xaxis = list(zeroline = FALSE),
+       yaxis = list(zeroline = FALSE))
+style.scatterplot
 # load in the data set diamonds 
-
+data("diamonds")
 
 # Use sample() and nrow() to only take a random sampling of 1000 rows from the
 # diamonds data and pass that into a new variable
-
+d <- diamonds[sample(nrow(diamonds), 1000), ]
 
 # Now create a new plot passing in your sample of a 1000 rows from the diamonds
 # data, set x, y, and z axis as carat, price, and depth.  
 # Finally add makers that is the titled "Clarity, and adds the Clarity of the 
 # diamonds
-
-
+p <- plot_ly(d, x = ~carat, y = ~price, z = ~depth) %>%
+  add_markers(text = ~paste("Clarity: ", clarity))
+p
 
 #Bonus 
 # For this you will make a graph with two differnet Y axis 
@@ -50,8 +58,13 @@
 # for this the font should be red, overlaying = "y", on the right side, 
 # and should have a title distinguishing it as the second y axis. 
 
-
-
+second.axis <- list(
+  tickfont = list(color = "red"),
+  overlaying = "y",
+  side = "right",
+  title = "second y axis"
+)
+second.axis
 # Now call the plot_ly() funcion and use pipe operators to have two add_line
 # functions. The first will have x values from 1:3 and y value of 10*(1:3)
 # The second will have x values from 2:4 and y values from 1:3.  Both shoud
@@ -59,7 +72,13 @@
 # layout() function and add a title for the graph, set the yaxis2 to the previous
 # variable and have a title for the xaxis  
 
-
+plot_ly() %>%
+  add_lines(x = ~1:3, y = ~10*(1:3), name = "slope of 10") %>%
+  add_lines(x = ~2:4, y = ~1:3, name = "slope of 1", yaxis = "y2") %>%
+  layout(
+    title = "Double Y Axis", yaxis2 = second.axis,
+    xaxis = list(title="x")
+  )
 
 
 
